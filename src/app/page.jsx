a40b60748cb2a8f65672b9e212a1a994363b753f
coelhoreinaldo <scoelhor7@gmail.com>
2023-05-27 14:49:53 -0300

    ✨ feat: finish

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,25 +32,32 @@ export default function Home() {
 
   return (
     <main className="flex items-center justify-center sm:justify-between h-screen w-full overflow-hidden ">
-      <form className="flex flex-col gap-4 sm:p-8 lg:p-16 xl:p-48 sm:w-1/2 justify-center h-full" onSubmit={handleSubmit}>
-      <h1 className="text-3xl sm:text-6xl font-bold w-full text-center">Faça seu login<span className="bg-gradient-to-r from-blue-400 via-indigo-400 to-orange-400 bg-clip-text text-transparent text-4xl sm:text-8xl">.</span></h1>
-        <label htmlFor="email">Email *
+      <form className="flex flex-col space-y-11 m-auto justify-center h-full" onSubmit={handleSubmit}>
+      <h1 className="text-3xl lg:text-5xl xl:text-6xl font-bold w-full text-center">Faça seu login<span className="bg-gradient-to-r from-indigo-400 via-purple-400 to-orange-400 bg-clip-text text-transparent text-4xl sm:text-8xl">.</span></h1>
+      <section className="flex flex-col gap-4">
+
+        <label htmlFor="email">Email
         {isInvalid === 'email' && <small className="text-red-400 font-bold"> Campo inválido.</small>}
         </label>
-        <input className="bg-zinc-900  outline-blue-400 focus:outline-offset-2" type="email" id="email"  onChange={ ({target}) => setEmail(target.value)}/>
+        <input className="bg-zinc-900 outline-indigo-400 p-2 rounded focus:outline-2 focus:outline shadow-lg focus:shadow-indigo-400" type="email" id="email"  onChange={ ({target}) => {setEmail(target.value); 
+        setIsInvalid([])}}/>
 
-        <label htmlFor="password">Password *</label>
-        <input className="bg-zinc-900  outline-blue-400 focus:outline-offset-2" type="password" id="password" onChange={ ({target}) => setPassword(target.value)}/>
-        {isInvalid === 'password' && <small className="text-red-400 font-bold">Campo inválido.</small>}
+        <label htmlFor="password">Password
+        {isInvalid === 'password' && <small className="text-red-400 font-bold"> Campo inválido.</small>}
+        </label>
+        <input className="bg-zinc-900  outline-indigo-400 p-2 rounded focus:outline-2 focus:outline shadow-lg focus:shadow-indigo-400" type="password" id="password" onChange={ ({target}) => {setPassword(target.value);
+        setIsInvalid([])}}/>
+        <Link href="#" className="underline text-blue-900 hover:text-blue-950 transition text-right">Esqueci minha senha</Link>
+        </section>
         
-        <Link href="#" className="underline text-blue-700 hover:text-blue-900 transition text-right">Esqueci minha senha</Link>
-        <button type="submit" className="bg-gradient-to-tr from-blue-400 via-indigo-400 to-orange-400 text-white p-2 rounded hover:from-orange-400 hover:via-indigo-400 hover:to-blue-400 transition">Entrar</button>
+        <section className="flex flex-col gap-4">
 
-        <Link href="#" className="underline text-blue-700 hover:text-blue-900 transition text-center">Ainda não tenho uma conta</Link>
+        <button type="submit" className="bg-gradient-to-tr from-indigo-400 via-purple-400 to-orange-300 text-white p-2 rounded hover:from-orange-300 hover:via-purple-400 hover:to-indigo-400 transition">Entrar</button>
+
+        <Link href="#" className="underline text-blue-900 hover:text-blue-950 transition text-center">Ainda não tenho uma conta</Link>
+        </section>
       </form>
       <div className="sm:w-1/2 h-screen shadow-custom bg-[url('./assets/pillars.png')] bg-auto bg-center opacity-60">
-      {/* <Image src={background} alt="pillars of creation" className="hidden  sm:block h-screen  z-10 opacity-100  shadow-custom"></Image> */}
-
       </div>
     </main>
   )
